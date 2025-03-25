93632ddff832c71aace90347052fb8b33721384a
Sofia <sfcrdsslv@gmail.com>
2025-03-24 23:42:26 -0300

    Finalizado

--- AtividadeLista/AtividadeLista/MainPage.xaml.cs
+++ AtividadeLista/AtividadeLista/MainPage.xaml.cs
@@ -9,35 +9,12 @@
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
 
         private void btnListaPersonagem_Clicked(object sender, EventArgs e)
         {
             Navigation.PushAsync(new ListaPersonagemJogoPage());
         }
 
-        private void btnPersonagem_Clicked(object sender, EventArgs e)
-        {
-            PersonagemJogo personagemJogo = new PersonagemJogo();
-
-            personagemJogo.Nome = "leef";
-            personagemJogo.Historia = "ffe";
-            personagemJogo.Poder = "kf";
-            personagemJogo.Caracteristicas = "orefer";
-            personagemJogo.Origem = "erfje";
-
-            Navigation.PushAsync(new PersonagemJogoPage() { BindingContext = personagemJogo });
-        }
     }
 
 }
--- AtividadeLista/AtividadeLista/PersonagemJogo.cs
+++ AtividadeLista/AtividadeLista/PersonagemJogo.cs
@@ -21,19 +21,27 @@ namespace AtividadeLista
                 List<PersonagemJogo> lista = new List<PersonagemJogo>();
                 lista.Add(new PersonagemJogo() { Nome = "Jones", Historia = "Professor do Fernando Prestes que sai em uma jornada para salvar seus colegas",
                 Poder = "Jones pode aprender a usar qualquer habilidade de qualquer pessoa", 
-                Caracteristicas = "Muito carisma e determinação, mas pouco cabelo", Origem = "Terra" });
+                Caracteristicas = "Muito carisma e determinação", Origem = "Terra" });
                 
                 lista.Add(new PersonagemJogo() { Nome = "Alex", Historia = "Professor do Fernando Prestes que foi corrompido",
                 Poder = "Alex consegue usar seu cabelo incrível para atacar e se defender", 
                 Caracteristicas = "Cabelos longos, loiros e sedosos, porém mortais", Origem = "Terra" }); 
                 
-                lista.Add(new PersonagemJogo() { Nome = "Fernando Prestes", Historia = "Entidade que deu origem a escola Fernando Prestes",
+                lista.Add(new PersonagemJogo() { Nome = "Fernando Prestes", Historia = "Entidade que deu origem a escola de prodígios Fernando Prestes",
                 Poder = "Dizem as lendas que ele é capaz de criar e destruir planetas inteiros com sua força", 
                 Caracteristicas = "Desconhecidas", Origem = "Desconhecida" });
                 
                 lista.Add(new PersonagemJogo() { Nome = "Maycon", Historia = "Ser maligno com motivações desconhecidas",
                 Poder = "Maycon é capaz de tudo, mas é conhecido por alguns como Deus da destruição", 
                 Caracteristicas = "Desconhecidas", Origem = "Desconhecida" });
+                
+                lista.Add(new PersonagemJogo() { Nome = "Monge", Historia = "Desconhecida",
+                Poder = "O Monge, no seu auge do poder, fez coisas que muitos dizem serem impossíveis ", 
+                Caracteristicas = "Desconhecidas", Origem = "Desconhecida" });
+
+                lista.Add(new PersonagemJogo() { Nome = "Deuses do Badmintom", Historia = "A dupla profissional responsável por criar o esporte sagrado badminton e que transcendeu os limites do jogo",
+                Poder = "Os deuses do badmintom podem resignificar esse esporte e tranformá-lo em um campo de batalha", 
+                Caracteristicas = "Profissionais de badminton", Origem = "Terra" });
                 return lista;
             }
         }
